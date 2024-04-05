f3cb42996b9f737cd1117d07f2d1a9cd2efba12b
wxy1203 <e0817822@u.nus.edu>
2024-04-05 11:31:20 +0800

    Create utils.py

---
+++ webscraper_details/utils.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+def make_request(url: str):
+    try:
+        response = requests.get(url)
+        response.encoding = 'utf-8'
+        response.raise_for_status()
+        return BeautifulSoup(response.content, 'html.parser')
+    except requests.RequestException as e:
+        print(f"Failed to retrieve page: {e}")
+        return None
+
+def get_default_result():
+    return {
+        'Terminal': "",
+        'Area': "",
+        'Level': "",
+        'Opening Hour': "",
+        'outlets': [],
+    }
+
+def create_new_row(domain, facility, terminal, area, level, opening_hour, description):
+    return pd.DataFrame({
+        'Domain': [domain],
+        'Facility/Service/Attraction': [facility],
+        'Terminal': [terminal],
+        'Area': [area],
+        'Level': [level],
+        'Opening Hour': [opening_hour],
+        'Description': [description]
+    })
